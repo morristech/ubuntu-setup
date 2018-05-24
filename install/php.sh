c1dddc0532a805d59edd6b2ca39da4964cd6249a
Fabiano Lima <fabiano@ingressocerto.com>
2018-05-24 19:03:31 -0300

    php xdebug

--- install/php.sh
+++ install/php.sh
@@ -4,3 +4,5 @@ sudo apt-get install php-mcrypt
 sudo apt-get install php-mysql
 sudo apt-get install php-mbstring
 sudo apt-get install php-pdo-sqlite
+sudo apt-get install php-curl
+sudo apt-get install php-xdebug
